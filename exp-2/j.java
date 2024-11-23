c31a2ade641f7404eabba69cafc7259e68703d0d
sambhavverma2004 <165543114+sambhavverma2004@users.noreply.github.com>
2024-11-23 09:20:50 +0530

    Update j.java

--- exp-2/j.java
+++ exp-2/j.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class CreditLimitCalculator {
+public class CreditLimitCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
